e13b20435688a6cfa9b616b62588a767880a95be
shulgaalexey <ShulgaAlexey@gmail.com>
2017-06-15 14:36:51 +0900

    Create RootDialog.cs

--- HelloTizenBot/Dialogs/RootDialog.cs
+++ HelloTizenBot/Dialogs/RootDialog.cs
@@ -23,7 +23,7 @@ namespace HelloTizenBot.Dialogs
             int length = (activity.Text ?? string.Empty).Length;
 
             // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+            await context.PostAsync($"STAGING You sent {activity.Text} which was {length} characters");
 
             context.Wait(MessageReceivedAsync);
         }
