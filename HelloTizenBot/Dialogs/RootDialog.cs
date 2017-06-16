2e04f4fabfb5520b7af25f46d9f311d60fb08c7d
shulga <a.shulga@samsung.com>
2017-06-16 15:30:08 +0900

    Switching to own LUIS
    Signed-off-by: shulga <a.shulga@samsung.com>

--- HelloTizenBot/Dialogs/RootDialog.cs
+++ HelloTizenBot/Dialogs/RootDialog.cs
@@ -23,12 +23,12 @@ namespace HelloTizenBot.Dialogs
         {
             var activity = await result as Activity;
             
-            await context.PostAsync($"Starting Luis query");
+            //await context.PostAsync($"Starting Luis query");
 
             try
             {
                 //LuisService luis = new LuisService(new LuisModelAttribute("86684636-488d-48b3-a4c6-233ef496d3d1", "5f3fca65f0a64d68ab6d4d474b1b0fa6", LuisApiVersion.V2, "westus", true, false, true, true));
-                LuisService luis = new LuisService(new LuisModelAttribute("86684636-488d-48b3-a4c6-233ef496d3d1", "5f3fca65f0a64d68ab6d4d474b1b0fa6"));
+                LuisService luis = new LuisService(new LuisModelAttribute("64f464ab-0f2a-4412-9be1-af3e7fca947b", "d14db6f08ac845cda0873c80437bbb97"));
                 var luisResult = await luis.QueryAsync(activity.Text, System.Threading.CancellationToken.None);
 
                 //await context.PostAsync($"Luis Result received .. Processing intents");
@@ -37,16 +37,16 @@ namespace HelloTizenBot.Dialogs
                 {
                     case "":
                     case "None":
-                        await context.PostAsync($"Please repeat your query. I did not understand...");
+                        await context.PostAsync($"Top Intent: None\nPlease repeat your query. I did not understand...");
                         break;
-                    case "Greeting":
-                        await context.PostAsync($"Greetings.. How may I serve you?");
+                    case "Greetings":
+                        await context.PostAsync($"Top Intent: Greeting\nHow may I serve you?");
                         break;
                     case "Home":
-                        await context.PostAsync($"Home.. What should I do for you?");
+                        await context.PostAsync($"Top Intent: Home\nWhat should I do for you?");
                         break;
                     default:
-                        await context.PostAsync($"Please repeat your query. I could not recognize your intent...");
+                        await context.PostAsync($"Top Intent: UNKNOWN\nPlease repeat your query. I could not recognize your intent...");
                         break;
                 }
             }
